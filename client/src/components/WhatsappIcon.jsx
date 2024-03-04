a1e0fbc428b1aec178d6accac3fa08dd6c8509b1
Abdu1-Rahman <abduofficial01@gmail.com>
2024-03-04 16:12:21 +0530

    changed some ui

--- client/src/components/WhatsappIcon.jsx
+++ client/src/components/WhatsappIcon.jsx
@@ -5,7 +5,7 @@ const WhatsappIcon = () => {
   const whatsappLink = 'https://api.whatsapp.com/send?phone=1234567890'; // Replace with your actual WhatsApp link
 
   return (
-    <div className='fixed bottom-7 right-7'>
+    <div className='fixed  bottom-7 right-7'>
       <a href={whatsappLink} target='_blank' rel='noopener noreferrer'>
         <img className='w-12 cursor-pointer' src={whatsAppIcon} alt='WhatsappIcon' />
       </a>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -91,35 +91,7 @@ const Home = () => {
           alt="pics"
           className="h-64 w-full object-cover transition-transform duration-200 ease-in-out transform hover:scale-110 overflow-hidden"
         />
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center">
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full p-2.5 g-lg-5 ps-10 text-sm border border-gray-300 rounded-full bg-gray-50 outline-none"
-            placeholder="Search"
-            required
-          />
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-3 dark:bg-indigo-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2"
-          >
-            <svg
-              className="w-4 h-4 text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </button>
-        </div>
+        
       </div>
       <div className="ml-3">
         <h6 className="flex gap-2 text-3xl font-medium mt-3 text-gray-800">Best Properties For Sale in <div className='text-indigo-700'>UK</div></h6>
